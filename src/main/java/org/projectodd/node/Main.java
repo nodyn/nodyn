2b4eaee5b52f501d82ace58133f92893d32b5267
Bob McWhirter <bob@mcwhirter.org>
2012-05-08 09:49:41 -0400

    Initial import.

---
+++ src/main/java/org/projectodd/node/Main.java
@@ -0,0 +1,10 @@
+package org.projectodd.node;
+
+public class Main {
+	
+	public static void main(String...args) {
+		Node node = new Node( args );
+		node.start();
+	}
+
+}
---
+++ src/main/java/org/projectodd/node/Node.java
@@ -0,0 +1,15 @@
+package org.projectodd.node;
+
+public class Node {
+	
+	private String[] args;
+
+	public Node(String...args) {
+		this.args = args;
+	}
+	
+	public void start() {
+		
+	}
+
+}
