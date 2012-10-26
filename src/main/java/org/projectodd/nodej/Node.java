2bb355c4107a0889c2ccf36b97dd411d52b739a4
Lance Ball <lball@redhat.com>
2012-10-26 08:27:33 -0400

    Stop @Ignore'ing the basic require('util') test

--- src/main/java/org/projectodd/nodej/Node.java
+++ src/main/java/org/projectodd/nodej/Node.java
@@ -6,24 +6,23 @@ import org.dynjs.runtime.GlobalObject;
 import org.dynjs.runtime.GlobalObjectFactory;
 
 public class Node {
-	
-	public static final String VERSION = "0.1.0";
+
+    public static final String VERSION = "0.1.0";
     private String[] args;
 
-	public Node(String...args) {
-		this.args = args;
-	}
-	
-	public void start(ExecutionContext executionContext) {
+    public Node(String... args) {
+        this.args = args;
+    }
+
+    public void start(ExecutionContext executionContext) {
         executionContext.getConfig().setGlobalObjectFactory(new GlobalObjectFactory() {
             @Override
             public GlobalObject newGlobalObject(DynJS runtime) {
                 final GlobalObject globalObject = new GlobalObject(runtime);
-	            globalObject.defineGlobalProperty("process", new Process(globalObject, Node.this.args));
+                globalObject.defineGlobalProperty("process", new Process(globalObject, Node.this.args));
                 return globalObject;
             }
         });
-	}
-	
+    }
 
 }
--- src/test/java/org/projectodd/nodej/UtilTest.java
+++ src/test/java/org/projectodd/nodej/UtilTest.java
@@ -4,14 +4,14 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.IOException;
 
+import org.dynjs.runtime.DynObject;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class UtilTest extends NodejTestSupport {
 
     @Test
-    @Ignore
     public void testRequireUtil() throws IOException {
-        assertThat(runtime.execute("require('util')")).isEqualTo(null);
+        assertThat(runtime.execute("require('util')").getClass()).isEqualTo(DynObject.class);
     }
 }
