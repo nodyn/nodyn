f2b1f2fe495856c8b6c424ff4e4d0d53ded24c13
Lance Ball <lball@redhat.com>
2012-11-27 15:50:45 -0500

    Use sigar for uname.

--- src/main/java/org/projectodd/nodej/bindings/os/GetOSType.java
+++ src/main/java/org/projectodd/nodej/bindings/os/GetOSType.java
@@ -5,21 +5,31 @@ import java.util.Map;
 
 import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SysInfo;
 
 public class GetOSType extends OsFunctionBinding {
     
     private static Map<String, String> names = new HashMap<String, String>();
+    private SysInfo sysInfo;
     static {
-        names.put("Mac OS X", "Darwin");
+        names.put("MacOSX", "Darwin");
     }
     
     public GetOSType(GlobalObject globalObject) {
         super(globalObject);
+        sysInfo = new SysInfo();
+        try {
+            sysInfo.gather(sigar);
+        } catch (SigarException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Object call(ExecutionContext context, Object self, Object... args) {
-        String name = System.getProperty("os.name");
+        String name = "unknown";
+        name = sysInfo.getName();
         if (names.get(name) != null) {
             return names.get(name);
         }
