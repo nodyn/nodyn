e29dead250a3e134dba22465dfb0c5225e5e204c
Lance Ball <lball@redhat.com>
2012-12-05 13:35:17 -0500

    Implement SlowBuffer.prototype.byteLength

--- src/main/java/org/projectodd/nodej/bindings/buffer/ByteLength.java
+++ src/main/java/org/projectodd/nodej/bindings/buffer/ByteLength.java
@@ -1,8 +1,10 @@
 package org.projectodd.nodej.bindings.buffer;
 
+import org.dynjs.exception.ThrowException;
 import org.dynjs.runtime.AbstractNativeFunction;
 import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
+import org.dynjs.runtime.Types;
 
 public class ByteLength extends AbstractNativeFunction {
     
@@ -12,7 +14,11 @@ public class ByteLength extends AbstractNativeFunction {
 
     @Override
     public Object call(ExecutionContext context, Object self, Object... args) {
-        return "12";
+        if (!(args[0] instanceof String)) {
+            throw new ThrowException(context, context.createTypeError("Argument must be a string."));
+        }
+        String string = Types.toString(context, args[0]);
+        return string.getBytes().length;
     }
 
 }
--- src/test/java/org/projectodd/nodej/bindings/buffer/SlowBufferTest.java
+++ src/test/java/org/projectodd/nodej/bindings/buffer/SlowBufferTest.java
@@ -2,8 +2,7 @@ package org.projectodd.nodej.bindings.buffer;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-import java.net.UnknownHostException;
-
+import org.dynjs.exception.ThrowException;
 import org.dynjs.runtime.DynObject;
 import org.dynjs.runtime.JSFunction;
 import org.junit.Test;
@@ -11,13 +10,26 @@ import org.projectodd.nodej.NodejTestSupport;
 
 public class SlowBufferTest extends NodejTestSupport {
     @Test
-    public void testSlowBuffer() throws UnknownHostException {
+    public void testSlowBuffer() {
         assertThat(eval("process.binding('buffer').SlowBuffer")).isInstanceOf(SlowBuffer.class);
         assertThat(eval("process.binding('buffer').SlowBuffer.prototype")).isInstanceOf(DynObject.class);
     }
 
     @Test
-    public void testByteLength() throws UnknownHostException {
+    public void testByteLength() {
         assertThat(eval("process.binding('buffer').SlowBuffer.byteLength")).isInstanceOf(JSFunction.class);
+        assertThat(eval("process.binding('buffer').SlowBuffer.byteLength('foo')")).isEqualTo(3);
+    }
+    
+    @Test
+    public void testByteLengthWithUnicode() {
+        eval("str = '\u00bd + \u00bc = \u00be';");
+        assertThat(eval("str.length == 9")).isEqualTo(true);
+        assertThat(eval("process.binding('buffer').SlowBuffer.byteLength(str, 'utf8')")).isEqualTo(12);
+    }
+    
+    @Test(expected = ThrowException.class)
+    public void testByteLengthFailsWithTypeError() {
+        assertThat(eval("process.binding('buffer').SlowBuffer.byteLength(8)"));
     }
 }
