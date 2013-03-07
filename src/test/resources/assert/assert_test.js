a88f1bdc6268865939e86833766a13894607c1bf
Lance Ball <lball@redhat.com>
2013-03-07 12:22:28 -0500

    Be sure to deploy our nodej module before running the tests

--- src/test/resources/assert/assert_test.js
+++ src/test/resources/assert/assert_test.js
@@ -1,20 +1,15 @@
 load("vertx.js");
 load("vertx_tests.js");
 
-java.lang.System.err.println("MODULE NAME: " + java.lang.System.getProperty("vertx.modulename"));
-
 function testAssertOK() {
   node_assert = require('assert');
   vassert.assertEquals(node_assert.ok(true), true);
   vassert.assertEquals(node_assert.ok(false), false);
   vassert.testComplete();
 }
-//var script = this;
-//var nodeConfig = {};
-//vertx.deployModule("org.projectodd#nodej#0.1.1.SNAPSHOT", nodeConfig, 1, function() {
-  initTests(this);
-//});
-// vertx.deployModule(java.lang.System.getProperty("vertx.modulename"), nodeConfig, 1, function() {
-//   initTests(script);
-// });
+var script = this;
+var nodeConfig = {};
+vertx.deployModule(java.lang.System.getProperty("vertx.modulename"), nodeConfig, 1, function() {
+  initTests(script);
+});
 
