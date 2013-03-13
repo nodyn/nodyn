18ccb8e2a39c859506de1033b62230d10983b10e
Lance Ball <lball@redhat.com>
2013-03-13 12:14:29 -0400

    Stub in console tests

---
+++ src/test/resources/console/console_test.js
@@ -0,0 +1,17 @@
+load('vertx.js');
+load('vertx_tests.js');
+
+function testConsoleLog() {
+  vassert.assertEquals('function', typeof console.log);
+  console.log('a log message');
+  vassert.testComplete();
+}
+
+function testConsoleInfo() {
+//  vassert.assertEquals('function', typeof console.info);
+//  console.info('an info message');
+  vassert.testComplete();
+}
+
+initTests(this);
+
