38d2ebe184105ac29f457b332116a8499ca68e08
Lance Ball <lball@redhat.com>
2013-03-20 16:21:17 -0400

    Accommodate new DynJSVerticleFactory structure

--- src/main/java/org/projectodd/nodej/NodeJSVerticleFactory.java
+++ src/main/java/org/projectodd/nodej/NodeJSVerticleFactory.java
@@ -7,30 +7,21 @@ import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
 import org.dynjs.runtime.GlobalObjectFactory;
 import org.dynjs.runtime.InitializationListener;
+import org.dynjs.vertx.DynJSVerticle;
 import org.dynjs.vertx.DynJSVerticleFactory;
+import org.vertx.java.platform.Verticle;
 
 public class NodeJSVerticleFactory extends DynJSVerticleFactory {
     private GlobalObjectFactory globalObjectFactory = new NodeJSGlobalObjectFactory();
 
-    protected ExecutionContext initializeRootContext() {
-        return ExecutionContext.createGlobalExecutionContext(getRuntime(), new InitializationListener()
-        {
-            @Override
-            public void initialize(ExecutionContext context) {
-                try {
-                    loadScript(context, "vertx.js");
-                    loadScript(context, "node.js");
-                } catch (FileNotFoundException e) {
-                    System.err.println("Missing file. Cannot initialize NodeJ.");
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
     @Override
     protected GlobalObjectFactory getGlobalObjectFactory() {
         return this.globalObjectFactory;
+    }
+
+    @Override
+    public Verticle createVerticle(String main) throws Exception {
+        return new NodeJSVerticle(this, main);
     }
 
     private class NodeJSGlobalObjectFactory extends DynJSGlobalObjectFactory {
@@ -41,6 +32,31 @@ public class NodeJSVerticleFactory extends DynJSVerticleFactory {
             global.defineGlobalProperty("Buffer", "foo");
             return global;
         }
+    }
+    
+    protected class NodeJSVerticle extends DynJSVerticle {
+
+        public NodeJSVerticle(DynJSVerticleFactory factory, String scriptName) {
+            super(factory, scriptName);
+        }
+        
+        @Override
+        protected ExecutionContext initializeRootContext() {
+            return ExecutionContext.createGlobalExecutionContext(factory.getRuntime(), new InitializationListener()
+            {
+                @Override
+                public void initialize(ExecutionContext context) {
+                    try {
+                        loadScript(context, "vertx.js");
+                        loadScript(context, "node.js");
+                    } catch (FileNotFoundException e) {
+                        System.err.println("Missing file. Cannot initialize NodeJ.");
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
     }
 
 }
