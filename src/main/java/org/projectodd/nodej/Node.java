38bcafd34f319e796338d3138b639025c45f0add
Lance Ball <lball@redhat.com>
2013-03-21 10:56:04 -0400

    Remove Node.DISPATCHER and unused code.

--- src/main/java/org/projectodd/nodej/Node.java
+++ src/main/java/org/projectodd/nodej/Node.java
@@ -1,67 +1,19 @@
 package org.projectodd.nodej;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
-import org.dynjs.runtime.JSFunction;
 import org.projectodd.nodej.bindings.buffer.BufferType;
-import org.projectodd.nodej.bindings.timers.ClearTimeout;
-import org.projectodd.nodej.bindings.timers.SetTimeout;
 
 public class Node {
 
     public static final String VERSION = "0.1.0";
-    public static final int MAX_THREADS = 100;
-    private static final ScheduledExecutorService DISPATCH = Executors.newScheduledThreadPool(MAX_THREADS);
-
     private String filename = "<eval>";
 
     public Node(final ExecutionContext context) {
         GlobalObject globalObject = context.getGlobalObject();
-        final ClearTimeout clearTimeout = new ClearTimeout(globalObject);
-        globalObject.defineGlobalProperty("setTimeout", new SetTimeout(globalObject, false));
-        globalObject.defineGlobalProperty("clearTimeout", clearTimeout);
-        globalObject.defineGlobalProperty("setInterval", new SetTimeout(globalObject, true));
-        globalObject.defineGlobalProperty("clearInterval", clearTimeout);
         globalObject.defineGlobalProperty("Buffer", new BufferType(globalObject));
     }
     
-    public static Future<Object> dispatch(final JSFunction func, final ExecutionContext context, final Object...args) {
-        Callable<Object> callable = new Callable<Object>() {
-            @Override
-            public Object call() throws Exception {
-                return context.call(func, null, args);
-            }
-        };
-        return DISPATCH.submit(callable);
-    }
-    
-    @SuppressWarnings("unchecked")
-    public static Future<Object> dispatchAt(final ExecutionContext context, long delay, boolean repeat, final JSFunction func, final Object...args) {
-        if (repeat) {
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    context.call(func, null, args);
-                }
-            };
-            return (Future<Object>) DISPATCH.scheduleWithFixedDelay(runnable, delay, delay, TimeUnit.MILLISECONDS);
-        } else {
-            Callable<Object> callable = new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    return context.call(func, null, args);
-                }
-            };
-            return DISPATCH.schedule(callable, delay, TimeUnit.MILLISECONDS);
-        }
-    }
-
     public String getDirname() {
         return System.getProperty("user.dir");
     }
