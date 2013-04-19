96e481018baca8766cd842bd710320cfe4eb5524
zznate <zznate.m@gmail.com>
2013-04-19 14:19:37 -0500

    compile-error for recent AsyncResult handler changes in vert.x core

--- src/test/java/org/projectodd/nodej/test/ScriptClassRunner.java
+++ src/test/java/org/projectodd/nodej/test/ScriptClassRunner.java
@@ -42,6 +42,7 @@ import org.junit.runner.manipulation.NoTestsRemainException;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
@@ -139,7 +140,7 @@ public class ScriptClassRunner extends Runner implements Filterable {
                 Handler<Message<JsonObject>> handler = new Handler<Message<JsonObject>>() {
                     @Override
                     public void handle(Message<JsonObject> msg) {
-                        JsonObject jmsg = msg.body;
+                        JsonObject jmsg = msg.body();
                         String type = jmsg.getString( "type" );
                         switch (type) {
                         case "done":
@@ -163,24 +164,26 @@ public class ScriptClassRunner extends Runner implements Filterable {
                         testLatch.countDown();
                     }
                 };
-                EventBus eb = mgr.getVertx().eventBus();
+                EventBus eb = mgr.vertx().eventBus();
                 eb.registerHandler( TESTRUNNER_HANDLER_ADDRESS, handler );
                 final CountDownLatch deployLatch = new CountDownLatch( 1 );
                 final AtomicReference<String> deploymentIDRef = new AtomicReference<>();
-                mgr.deployVerticle( this.filename, conf, new URL[0], 1, null, new Handler<String>() {
-                    public void handle(String deploymentID) {
-                        deploymentIDRef.set( deploymentID );
-                        deployLatch.countDown();
-                    }
+                mgr.deployVerticle( this.filename, conf, new URL[0], 1, null, new Handler<AsyncResult<String>>() {
+                  @Override
+                  public void handle(AsyncResult<String> event) {
+                    deploymentIDRef.set( event.result() );
+                    deployLatch.countDown();
+                  }
                 } );
                 waitForLatch( deployLatch );
                 waitForLatch( testLatch );
                 eb.unregisterHandler( TESTRUNNER_HANDLER_ADDRESS, handler );
                 final CountDownLatch undeployLatch = new CountDownLatch( 1 );
-                mgr.undeploy( deploymentIDRef.get(), new Handler<Void>() {
-                    public void handle(Void v) {
-                        undeployLatch.countDown();
-                    }
+                mgr.undeploy( deploymentIDRef.get(), new Handler<AsyncResult<Void>>() {
+                  @Override
+                  public void handle(AsyncResult<Void> event) {
+                    undeployLatch.countDown();
+                  }
                 } );
                 waitForLatch( undeployLatch );
                 if (failure.get() != null) {
