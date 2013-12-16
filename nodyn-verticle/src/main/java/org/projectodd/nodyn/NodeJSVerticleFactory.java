786393cb92c04c1ba9fbca1a7e2229fbd7853058
Lance Ball <lball@redhat.com>
2013-12-16 13:50:33 -0500

    Adjust for DynJSVerticleFactory changes

--- nodyn-verticle/src/main/java/org/projectodd/nodyn/NodeJSVerticleFactory.java
+++ nodyn-verticle/src/main/java/org/projectodd/nodyn/NodeJSVerticleFactory.java
@@ -1,33 +1,28 @@
 package org.projectodd.nodyn;
 
-import org.dynjs.runtime.DynJS;
-import org.dynjs.runtime.DynObject;
-import org.dynjs.runtime.ExecutionContext;
-import org.dynjs.runtime.GlobalObject;
-import org.dynjs.runtime.GlobalObjectFactory;
-import org.dynjs.runtime.InitializationListener;
+import org.dynjs.Config;
+import org.dynjs.runtime.*;
 import org.dynjs.vertx.DynJSVerticle;
 import org.dynjs.vertx.DynJSVerticleFactory;
+import org.projectodd.nodyn.buffer.BufferType;
 import org.projectodd.nodyn.modules.NpmModuleProvider;
 import org.projectodd.nodyn.util.QueryString;
-import org.projectodd.nodyn.buffer.BufferType;
 import org.vertx.java.platform.Verticle;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class NodeJSVerticleFactory extends DynJSVerticleFactory {
-    private GlobalObjectFactory globalObjectFactory = new NodeJSGlobalObjectFactory();
     private String filename;
-
-    @Override
-    protected GlobalObjectFactory getGlobalObjectFactory() {
-        return this.globalObjectFactory;
-    }
 
     @Override
     public Verticle createVerticle(String main) throws Exception {
         this.filename = main;
-        return new NodeJSVerticle(runtime, main);
+        Config config = new Config(getClassLoader());
+        config.setGlobalObjectFactory(new NodeJSGlobalObjectFactory());
+        return new NodeJSVerticle(new DynJS(config), main);
     }
 
     private class NodeJSGlobalObjectFactory extends DynJSGlobalObjectFactory {
@@ -40,27 +35,27 @@ public class NodeJSVerticleFactory extends DynJSVerticleFactory {
             node.put("QueryString", new QueryString(global));
             global.defineGlobalProperty("nodyn", node);
             global.defineGlobalProperty("__filename", filename);
-            new NpmModuleProvider(global);
             return global;
         }
     }
-    
+
     protected class NodeJSVerticle extends DynJSVerticle {
 
         public NodeJSVerticle(DynJS runtime, String scriptName) {
             super(runtime, scriptName);
         }
-        
+
         @Override
         protected ExecutionContext initializeRootContext() {
-            return ExecutionContext.createGlobalExecutionContext(runtime, new InitializationListener()
-            {
+            final DynJS runtime = getRuntime();
+            return ExecutionContext.createGlobalExecutionContext(runtime, new InitializationListener() {
                 @Override
                 public void initialize(ExecutionContext context) {
                     InputStream is = runtime.getConfig().getClassLoader().getResourceAsStream("node.js");
                     if (is != null) {
                         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
                         runtime.newRunner().withContext(context).withSource(reader).evaluate();
+                        new NpmModuleProvider(context.getGlobalObject());
                         try {
                             is.close();
                         } catch (IOException e) {
