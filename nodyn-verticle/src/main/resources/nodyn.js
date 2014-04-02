09c82b68607439613522061429a8fb388914b057
Bob McWhirter <bob@mcwhirter.org>
2014-04-02 15:07:17 -0400

    More correct, but not any less wrong.
    Actually require EventEmitter into the nodyn.js file so it's visible,
    even though it seemed to be visible already.

--- nodyn-verticle/src/main/resources/nodyn.js
+++ nodyn-verticle/src/main/resources/nodyn.js
@@ -1,4 +1,5 @@
-var util = NativeRequire.require('util');
+var util          = NativeRequire.require('util');
+var EventEmitter  = NativeRequire.require('events').EventEmitter;
 
 module.exports.makeEventEmitter = function(ctor) {
   util._extend(ctor.prototype, EventEmitter.prototype);
