f0d329e082faedc0df8b4a82e27ebacdc0a1bfe1
Bob McWhirter <bob@mcwhirter.org>
2014-04-22 09:22:54 -0400

    Make loop.sh a tad more portable.

--- support/loop.sh
+++ support/loop.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-MVN=/usr/local/bin/mvn
+#MVN=/usr/local/bin/mvn
+if [ "$MVN" = '' ] ; then
+  MVN=mvn
+fi
 OUTFILE=./tmp/build
 TASK="clean verify"
 
@@ -11,8 +14,9 @@ if [ ! -e ./tmp ] ; then
   mkdir tmp
 fi
 
-for i in {1..1000}
+for i in {1..100}
 do
   #${MVN} ${TASK} ${OPTS} > ${OUTFILE}-${i}.log 2>&1
+  echo ${MVN} ${TASK} -Dtest.pattern="**/netPauseSpec.js"
   ${MVN} ${TASK} -Dtest.pattern="**/netPauseSpec.js" > ${OUTFILE}-${i}.log 2>&1
 done
