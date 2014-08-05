97062132cf7484841896061ebe8d99448e5a249b
Bob McWhirter <bob@mcwhirter.org>
2014-08-05 17:02:09 -0400

    Clean up some imports; inject some license headers.

--- src/main/java/io/nodyn/tty/WriteStream.java
+++ src/main/java/io/nodyn/tty/WriteStream.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.nodyn.tty;
 
 import io.netty.channel.Channel;
@@ -5,7 +21,6 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.nodyn.netty.pipe.NioOutputStreamChannel;
 import io.nodyn.process.NodeProcess;
-import io.nodyn.stream.StreamEventsHandler;
 import io.nodyn.stream.StreamWrap;
 import io.nodyn.tcp.DataEventHandler;
 
