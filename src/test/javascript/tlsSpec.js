862db0ce4ebfd6bd6a881a4ac8f344c5ae24bbdc
Bob McWhirter <bob@mcwhirter.org>
2014-09-08 10:36:58 -0400

    Fix case of directory to keys.  Add another non-working, but not-failing test. Yay TDD.

--- src/test/javascript/tlsSpec.js
+++ src/test/javascript/tlsSpec.js
@@ -4,15 +4,39 @@ var fs     = require('fs');
 
 describe('tls', function(){
 
+  var serverKey;
+  var serverCert;
+
+  beforeEach( function() {
+    helper.testComplete(false);
+    serverKey  = fs.readFileSync( './keys/DSA/serverkey.pem');
+    serverCert = fs.readFileSync( './keys/DSA/servercert.pem');
+  })
+
   it( 'should allow creation of a server with key and cert', function() {
-    var serverKey  = fs.readFileSync( './keys/dsa/serverkey.pem');
-    var serverCert = fs.readFileSync( './keys/dsa/servercert.pem');
     var server = tls.createServer( {
       key: serverKey,
       passphrase: 'server',
       cert: serverCert,
-    }, function(connection) {
+    },
+    function(connection) {
     })
+  });
+
+  it( 'should allow creation of a server that can listen with key and cert', function() {
+    waitsFor(helper.testComplete, "server to be listening", 5000);
+    var server = tls.createServer( {
+      key: serverKey,
+      passphrase: 'server',
+      cert: serverCert,
+    },
+    function(connection) {
+    })
+
+    server.listen(8181, function() {
+      server.close();
+      helper.testComplete(true);
+    });
   });
 
 });
